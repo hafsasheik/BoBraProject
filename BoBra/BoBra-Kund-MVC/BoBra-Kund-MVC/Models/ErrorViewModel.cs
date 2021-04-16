49b82c505d12a3d76e65091120b6cb4d6f396871
FLiindberg <filip.lindberg97@gmail.com>
2021-04-16 11:15:18 +0200

    bobrakundmvc

---
+++ BoBra/BoBra-Kund-MVC/BoBra-Kund-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BoBra_Kund_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
